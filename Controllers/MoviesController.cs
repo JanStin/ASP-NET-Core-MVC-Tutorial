80e824c30543708eefe48e6e9192266a70c17979
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-09-13 16:31:59 +0300

    Added genre search

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,8 +20,10 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            IQueryable<string> genreQuery = from m in _context.Movies orderby m.Genre select m.Genre;
+
             var movies = from m in _context.Movies select m;
 
             if(!String.IsNullOrWhiteSpace(searchString))
@@ -29,7 +31,18 @@ namespace MvcMovie.Controllers
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            if(!String.IsNullOrWhiteSpace(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
